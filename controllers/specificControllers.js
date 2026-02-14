733d102d8385f850db9b61d41bdfbb08d0ebee56
mansuur-iman <imaninspires1@gmail.com>
2026-02-14 08:37:56 +0300

    add special controllers that handle search and other special controls

---
+++ controllers/specificControllers.js
@@ -0,0 +1,71 @@
+const db = require("../db/queries");
+
+const { query, validationResult } = require("express-validator");
+
+const validateSearch = [
+  query("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Search query cannot be empty")
+    .isLength({ max: 50 })
+    .withMessage("Search query is too long"),
+];
+
+async function getProductWithItsCategory(req, res) {
+  try {
+    const result = await db.getAllProductsWithCategory();
+    res.render("home", {
+      title: "Inventory Dashboard",
+      result: result,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching dashboard data");
+  }
+}
+const searchTheProduct = [
+  ...validateSearch,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        console.error(errors.array());
+        return res.render("search", {
+          title: "Search result",
+          searchedProduct: [],
+          message: errors
+            .array()
+            .map((e) => e.msg)
+            .join(", "),
+        });
+      }
+
+      const { name } = req.query;
+
+      const searchedProduct = await db.searchProduct(name);
+
+      if (searchedProduct.length === 0) {
+        return res.render("search", {
+          title: "Search result",
+          searchedProduct: [],
+          message: "Product not found",
+        });
+      }
+
+      res.render("search", {
+        title: "Searched result",
+        searchedProduct,
+        message: null,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server error while searching product");
+    }
+  },
+];
+
+module.exports = {
+  getProductWithItsCategory,
+  searchTheProduct,
+};
